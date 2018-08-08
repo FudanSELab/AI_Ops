3918a7a8be5f06157328eee7241e5f0cc0dc9a42
Ji Chao <chaojifudan@outlook.com>
2018-08-08 15:42:38 +0800

    add collector demo

---
+++ etl/log_collector/1-services/rest-service-collector/src/main/java/hello/HelloController.java
@@ -0,0 +1,72 @@
+package hello;
+
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.atomic.AtomicLong;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.AsyncRestTemplate;
+import org.springframework.web.client.RestTemplate;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.util.concurrent.ListenableFuture;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+public class HelloController {
+
+	private static final Logger log = LoggerFactory.getLogger(Application.class);
+	@Autowired
+	private RestTemplate restTemplate;
+
+	@Autowired
+	private AsyncRestTemplate asyncRestTemplate;
+
+
+	@Autowired
+	private KafkaTemplate kafkaTemplate;
+
+	@RequestMapping(value = "api/v1/spans", method ={RequestMethod.POST,RequestMethod.GET})
+	public String handle_collect(@RequestBody String info) {
+		System.out.println("[===] HelloController - handle_collect");
+		System.out.println("==========HelloController - handle_collect============");
+		System.out.println(info);
+		System.out.println("==========================");
+
+
+		ListenableFuture future = kafkaTemplate.send("app_log", info);
+		future.addCallback(o -> System.out.println("send-success: " + info),
+				throwable -> System.out.println("failed: " + info));
+
+
+		return "---------post------------";
+		
+	}
+
+
+	@RequestMapping(value = {"api/**", "**/spans"}, method ={RequestMethod.POST,RequestMethod.GET})
+	public String handle_collect_2(@RequestBody String info) {
+		System.out.println("[===] HelloController - handle_collect_2");
+		System.out.println("==========HelloController - handle_collect_2============");
+		System.out.println(info);
+		System.out.println("==========================");
+		return "---------post span any------------";
+		
+	}
+
+
+
+	@RequestMapping(value = "**", method ={RequestMethod.POST,RequestMethod.GET})
+	public String handle_collect_any(@RequestBody String info) {
+		System.out.println("[===] HelloController - handle_collect_any");
+		System.out.println("==========HelloController - handle_collect_any============");
+		System.out.println(info);
+		System.out.println("==========================");
+		return "---------post any------------";
+		
+	}
+}
---
+++ etl/log_collector/1-services/rest-service-end/src/main/java/hello/GreetingController.java
@@ -0,0 +1,34 @@
+package hello;
+
+import java.util.concurrent.atomic.AtomicLong;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class GreetingController {
+
+	private static final Logger log = LoggerFactory.getLogger(Application.class);
+
+    private final AtomicLong counter = new AtomicLong();
+
+    @RequestMapping("/greeting")
+    public Greeting greeting(@RequestParam(value="cal", defaultValue="50") String cal) throws Exception {
+
+    	Greeting value = new Greeting();
+
+        return value;
+    }
+    
+    @RequestMapping("/test")
+    public String test(@RequestParam(value="cal", defaultValue="50") String cal) throws Exception {
+    	// log.info(cal);
+
+    	double cal2 = Math.abs(Double.valueOf(cal)-50); 
+    	
+        return String.valueOf(cal2);
+    }
+}
