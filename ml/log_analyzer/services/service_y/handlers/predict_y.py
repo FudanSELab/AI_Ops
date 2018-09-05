2b86d86bfe27124d10081ddf54925fd764af275e
Ji Chao <chaojifudan@outlook.com>
2018-09-05 17:21:41 +0800

    Add a microservice to train&predict&Verify Model Y.

--- ml/log_analyzer/services/service_y/handlers/predict_y.py
+++ ml/log_analyzer/services/service_y/handlers/predict_y.py
@@ -1,29 +1,19 @@
 from __future__ import absolute_import
 from __future__ import division
 from __future__ import print_function
-import pandas as pd
 import tensorflow as tf
-import argparse
 import data_load
-import columns
 import data_save
-
-parser = argparse.ArgumentParser()
-parser.add_argument('--batch_size', default=100, type=int, help='batch size')
-parser.add_argument('--train_steps', default=5, type=int, help='number of training steps')
+import columns
+import pandas as pd
 
 
-def get_features_to_be_predicted():
-    predict_feature = ['212212', 1, 2, 3, 4, 5, 6, 7]
+def get_features_to_be_predicted(new_feature_array_with_id):
     column = columns.CSV_FEATURE_ORIGINAL
-    data_item = tuple(predict_feature)
+    data_item = tuple(new_feature_array_with_id)
     data = [data_item]
-    print(data)
-    print(column)
     frame = pd.DataFrame(data, columns=column)
     feature, feature_id = frame, frame.pop("feature_id")
-
-    print(feature_id)
     return feature, feature_id
 
 
@@ -31,6 +21,7 @@ def predict_input_fn(features, batch_size):
     """An input function for evaluation or prediction"""
     features = dict(features)
     inputs = features
+
     # Convert the inputs to a Dataset.
     dataset = tf.data.Dataset.from_tensor_slices(inputs)
     # Batch the examples
@@ -40,19 +31,17 @@ def predict_input_fn(features, batch_size):
     return dataset
 
 
-def main(argv):
-
-    args = parser.parse_args(argv[1:])
+def predict(new_feature_array_with_id):
 
     # Get Feature Columns
-    feature_columns = data_load.get_feature_column("data/original_features.csv")
+    feature_columns = data_load.get_feature_column("../data/original_features.csv")
     # Build 2 hidden layer DNN with 20, 20 units respectively.
     checkpointing_config = tf.estimator.RunConfig(
         save_checkpoints_secs=60,  # Save checkpoints every 60 seconds.
         keep_checkpoint_max=10,  # Retain the 10 most recent checkpoints.
     )
     classifier = tf.estimator.DNNClassifier(
-        model_dir="model/y_dnn",
+        model_dir="../model/y_dnn",
         config=checkpointing_config,
         feature_columns=feature_columns,
         # Two hidden layers of 20 nodes each.
@@ -60,11 +49,11 @@ def main(argv):
         # The model must choose between 2 classes.
         n_classes=2)
 
-    data_to_be_predicted, feature_id = get_features_to_be_predicted()
+    data_to_be_predicted, feature_id = get_features_to_be_predicted(new_feature_array_with_id)
 
     predictions = classifier.predict(
         input_fn=lambda: predict_input_fn(data_to_be_predicted,
-                                          batch_size=args.batch_size)
+                                          batch_size=100)
     )
 
     predictions = list(predictions)
@@ -82,18 +71,7 @@ def main(argv):
         data_set.append(new_data_item)
         print(new_data_item)
 
-    print(data_set)
-
     data_save.write_to_csv(
-        "data/y2.csv",
+        "../data/y2.csv",
         data=data_set,
         header=None)
-    #
-    # for i range len(predictions_list:
-    #     print(pre.get("class_ids")[0])
-    # Write the predictions into the y2.csv
-
-
-if __name__ == '__main__':
-    tf.logging.set_verbosity(tf.logging.INFO)
-    tf.app.run(main)
---
+++ ml/log_analyzer/services/service_y/util/columns.py
@@ -0,0 +1,90 @@
+
+CSV_FEATURE_ORIGINAL = [
+                "feature_id",
+                "x1",
+                "x2",
+                "x3",
+                "x4",
+                "x5",
+                "x6",
+                "x7",
+]
+
+# x1 - x7 features and labeled by developer.
+CSV_FEATURE_AND_LABEL_RESULT = [
+                    "feature_id",
+                    "x1",
+                    "x2",
+                    "x3",
+                    "x4",
+                    "x5",
+                    "x6",
+                    "x7",
+                    "y1"
+]
+
+# x1 - x7 features and estimated by model.
+CSV_FEATURE_AND_ESTIMATE_RESULT = [
+                    "feature_id",
+                    "x1",
+                    "x2",
+                    "x3",
+                    "x4",
+                    "x5",
+                    "x6",
+                    "x7",
+                    "y2"
+]
+
+# x1 - x7 features and verified by test case.
+CSV_FEATURE_AND_VERIFIED_RESULT = [
+                    "feature_id",
+                    "x1",
+                    "x2",
+                    "x3",
+                    "x4",
+                    "x5",
+                    "x6",
+                    "x7",
+                    "y3"
+]
+
+# x1 - x7 features and delta labeled by developer.
+CSV_FEATURE_AND_LABELED_DELTA = [
+                    "feature_id",
+                    "x1",
+                    "x2",
+                    "x3",
+                    "x4",
+                    "x5",
+                    "x6",
+                    "x7",
+                    "d1"
+]
+
+# x1 - x7 features and delta estimated by model.
+CSV_FEATURE_AND_ESTIMATED_DELTA = [
+                    "feature_id",
+                    "x1",
+                    "x2",
+                    "x3",
+                    "x4",
+                    "x5",
+                    "x6",
+                    "x7",
+                    "d2"
+]
+
+# x1 - x7 features and delta estimated by model.
+CSV_FEATURE_AND_VERIFIED_DELTA = [
+                    "feature_id",
+                    "x1",
+                    "x2",
+                    "x3",
+                    "x4",
+                    "x5",
+                    "x6",
+                    "x7",
+                    "d3"
+]
+
