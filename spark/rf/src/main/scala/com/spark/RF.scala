93db5b56c3e96c2a61b68dc1a4727e1f44de5909
Ji Chao <chaojifudan@outlook.com>
2018-11-18 14:07:39 +0800

    Add code for RF

--- spark/rf/src/main/scala/com/spark/RF.scala
+++ spark/rf/src/main/scala/com/spark/RF.scala
@@ -1,22 +1,22 @@
 package com.spark
 
-import org.apache.spark.ml.classification.{DecisionTreeClassificationModel, DecisionTreeClassifier}
+import org.apache.spark.ml.{Pipeline, PipelineModel}
+import org.apache.spark.ml.classification.{DecisionTreeClassificationModel, RandomForestClassifier}
 import org.apache.spark.ml.evaluation.MulticlassClassificationEvaluator
 import org.apache.spark.ml.feature.VectorAssembler
 import org.apache.spark.ml.tuning.{ParamGridBuilder, TrainValidationSplit}
-import org.apache.spark.ml.{Pipeline, PipelineModel}
 import org.apache.spark.sql.{DataFrame, SparkSession}
+
 import scala.util.Random
 
-
-object DT extends App {
+object RF extends App {
   println("Hello, World!")
   //执行程序之前首先进行以下设定
   val master = "yarn"
-  val appName = "Spark Decision Tree"
+  val appName = "Spark Random Forest"
   val filePath = "hdfs://10.141.211.173:8020/user/admin/mock.csv"
 
-  println("[Run]Decision Main")
+  println("[Run]Random Forest Main")
 
   // 设置Spark运行时的参数
   val spark = SparkSession
@@ -50,15 +50,14 @@ object DT extends App {
   // 分割训练集和测试集合
   val Array(trainingData, testData) = featureAndLabel.randomSplit(Array(0.8, 0.2))
 
-  // 建立决策树模型，设置其参数
-  val dt = new DecisionTreeClassifier()
-    .setSeed(Random.nextLong())
-    .setLabelCol("y1")             //在这里设置label列
-    .setFeaturesCol("features")    //在这里设置feature列
-    .setPredictionCol("prediction")
+  // Train a RandomForest model.
+  val rf = new RandomForestClassifier()
+    .setLabelCol("y1")
+    .setFeaturesCol("features")
+    .setNumTrees(10)
 
   // 组装Pipeline并进行模型训练，注意这个模型是是Pipeline整个流水线的模型
-  val pipeline = new Pipeline().setStages(Array(dt))
+  val pipeline = new Pipeline().setStages(Array(rf))
   val pipelineModel = pipeline.fit(trainingData)
 
   // 用模型来计算测试数据
@@ -75,31 +74,28 @@ object DT extends App {
   println("Test Error = " + (1.0 - accuracy))
 
   // 把整个Pipeline模型的第一部分拿出来（pipeline根据之前的设定第一个为决策树模型），输出决策树判别细节
-  val treeModel = pipelineModel.stages(0).asInstanceOf[DecisionTreeClassificationModel]
-  println("Learned classification tree model:\n" + treeModel.toDebugString)
+  val treeModel = pipelineModel.stages(0).asInstanceOf[RandomForestClassifier]
 
   //模型的读取与存储
   pipelineModel.save("model/dt/pipeline_model")
-  val samePipelineModel = PipelineModel.load("model/dt/pipeline_model")
+  val samePipelineModel = PipelineModel.load("model/rf/pipeline_model")
 
   // 寻找最佳超参数：一下部分用于寻找最优超参数
   // We use a ParamGridBuilder to construct a grid of parameters to search over.
   // TrainValidationSplit will try all combinations of values and determine best model using
   // the evaluator.
   val paramGrid = new ParamGridBuilder()
-    .addGrid(dt.impurity, Seq("gini", "entropy"))        //不纯度函数
-    .addGrid(dt.maxDepth, Seq(5, 20))      //树的最大深度尝试
-    .addGrid(dt.maxBins, Seq(40, 200))      //
-    .addGrid(dt.minInfoGain, Seq(0.0, 0.05)) //
-    .addGrid(dt.maxDepth, Seq(5, 20))      //树的最大深度尝试
-    .addGrid(dt.minInstancesPerNode, Seq(10, 30))  //每个节点的最少元素数量
-    .build()                                             //还可以有很多的超参数
+    .addGrid(rf.maxDepth, Seq(5, 20))      //树的最大深度尝试
+    .addGrid(rf.maxDepth, Seq(5, 20))      //树的最大深度尝试
+    .build()
+
   // In this case the estimator is simply the linear regression.
   // A TrainValidationSplit requires an Estimator, a set of Estimator ParamMaps, and an Evaluator.
   val multiclassEval = new MulticlassClassificationEvaluator()
     .setLabelCol("y1")
     .setPredictionCol("prediction")
     .setMetricName("accuracy")
+
   val trainValidationSplit = new TrainValidationSplit()
     .setSeed(Random.nextLong())
     .setEstimator(pipeline)
@@ -130,5 +126,3 @@ object DT extends App {
     .show()
 
 }
-
-
