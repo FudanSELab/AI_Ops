413780b9e1980ec9b24f0d7f2df44d8c1da21fea
Ji Chao <chaojifudan@outlook.com>
2018-12-05 14:12:30 +0800

    Feature Selection

--- ml/python-preprocessing/feature_selection.py
+++ ml/python-preprocessing/feature_selection.py
@@ -4,6 +4,9 @@ from sklearn.feature_selection import chi2
 
 input_after_dimensionless_path = "transform/final_after_dimensionless.csv"
 input_data_y_column = "new_trace_y.y_issue_ms"
+input_data_y1_column = "new_trace_y.y_exec_result"
+input_data_y3_column = "new_trace_y.y_issue_dim_content"
+input_data_y4_column = "new_trace_y.y_issue_dim_type"
 
 output_after_feature_selection_path = "transform/final_after_feature_selection.csv"
 
@@ -11,10 +14,26 @@ data_after_feature_extraction = pd.read_csv(input_after_dimensionless_path,
                                             header=0,
                                             index_col=0)
 
+data_after_feature_extraction.pop(input_data_y1_column)
+data_after_feature_extraction.pop(input_data_y3_column)
+data_after_feature_extraction.pop(input_data_y4_column)
+
 X, y = data_after_feature_extraction, data_after_feature_extraction.pop(input_data_y_column)
 
-model_cq = SelectKBest(chi2, k=200)  # Select k best features
+model_cq = SelectKBest(chi2, k=100)  # Select k best features
 after_data = model_cq.fit_transform(X.values, y)
 
-# TODO: array to data frame
+selected_feature_indexs = model_cq.get_support(True)
+
+selected_column = data_after_feature_extraction.columns[selected_feature_indexs]
+
+data_after_feature_extraction = data_after_feature_extraction[selected_column]
+data_after_feature_extraction[input_data_y_column] = y
+
+print(output_after_feature_selection_path, "has", len(data_after_feature_extraction.keys()), "columns")
+
+# Output the result file.
+data_after_feature_extraction.to_csv(output_after_feature_selection_path)
+
+
 
