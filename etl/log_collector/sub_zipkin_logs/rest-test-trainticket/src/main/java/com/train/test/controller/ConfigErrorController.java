e9d8bed57cc48a063f8285e7a25686a02fab83fc
hechuan73 <1044856164@qq.com>
2018-12-11 14:22:53 +0800

    Description: complete the config test case codes.

--- etl/log_collector/sub_zipkin_logs/rest-test-trainticket/src/main/java/com/train/test/controller/ConfigErrorController.java
+++ etl/log_collector/sub_zipkin_logs/rest-test-trainticket/src/main/java/com/train/test/controller/ConfigErrorController.java
@@ -1,9 +1,20 @@
 package com.train.test.controller;
 
+import com.train.test.services.ConfigErrorService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping
+@RequestMapping("/config")
 public class ConfigErrorController {
+
+    @Autowired
+    private ConfigErrorService configErrorService;
+
+    @GetMapping("/errorTest")
+    public void testConfigError() throws Exception {
+        configErrorService.testConfigError();
+    }
 }
