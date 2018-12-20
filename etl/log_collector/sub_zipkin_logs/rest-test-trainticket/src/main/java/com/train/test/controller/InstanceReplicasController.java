05e7614b0d2f7e89467b374812be3ae13933615f
liuZOZO <308762585@qq.com>
2018-12-20 18:02:22 +0800

    dev

--- etl/log_collector/sub_zipkin_logs/rest-test-trainticket/src/main/java/com/train/test/controller/InstanceReplicasController.java
+++ etl/log_collector/sub_zipkin_logs/rest-test-trainticket/src/main/java/com/train/test/controller/InstanceReplicasController.java
@@ -1,27 +1,11 @@
 package com.train.test.controller;
 
-import com.train.test.domain.InitCacheDataResponse;
-import com.train.test.entity.instance.FlowTestResult;
-import com.train.test.entity.instance.ServiceReplicasSetting;
-import com.train.test.entity.instance.SetServiceReplicasRequest;
-import com.train.test.entity.instance.SetServiceReplicasResponse;
-import com.train.test.services.ConfigErrorService;
 import com.train.test.services.InstanceErrorService;
-import com.train.test.utils.ArrangeInStanceNum;
-import com.train.test.utils.BookingFlowPassServiceMapUtil;
-import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.client.RestTemplate;
 
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
-
-import java.util.concurrent.TimeUnit;
 
 @RestController
 public class InstanceReplicasController {
