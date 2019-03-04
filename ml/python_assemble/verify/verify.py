a4f58f43bb9969bb166cba9742145f8dace0249e
Ji Chao <chaojifudan@outlook.com>
2019-03-04 08:55:35 +0800

    verify

--- ml/python_assemble/verify/verify.py
+++ ml/python_assemble/verify/verify.py
@@ -1,4 +1,7 @@
 import pandas as pd
+from imblearn.over_sampling import RandomOverSampler
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.utils import shuffle
 
 
 def verify():
@@ -29,5 +32,56 @@ def verify():
     df.to_csv("verify.csv")
 
 
+def fetch():
+    df = pd.read_csv("verify.csv", header=0, index_col="trace_id")
+    df = df.loc[df["y_final_result"] == 1]
+    df.to_csv("fault.csv")
+
+
+def split(df: pd.DataFrame):
+    train = df.sample(frac=0.8)
+    test = df.drop(train.index)
+    return train, test
+
+
+# (Model_1 Model_2共用)按照y_name对数据进行过采样操作
+def sampling(df_raw: pd.DataFrame, y_name):
+    x, y = df_raw, df_raw.pop(y_name)
+    x_keys = x.keys()  # Save x-keys.
+    x_res, y_res = RandomOverSampler().fit_resample(x, y)
+    df_new_x = pd.DataFrame(data=x_res, columns=x_keys)
+    df_new_x[y_name] = y_res
+    df_new_x = shuffle(df_new_x)
+    return df_new_x
+
+
+def train():
+    df = pd.read_csv("verify.csv", header=0, index_col="trace_id")
+    df.pop("y_issue_dim_type")
+    df.pop("y_issue_ms")
+
+
+
+    train, test = split(df)
+
+    train = sampling(train, "y_final_result")
+
+    x, y = train, train.pop("y_final_result")
+    clf = RandomForestClassifier(min_samples_leaf=5000, n_estimators=3)
+    clf.fit(x, y)
+
+    real_x, real_y = test, test.pop("y_final_result")
+
+    pred_y = clf.predict(real_x)
+
+    count = 0
+    for i in range(len(real_y)):
+        print(pred_y[i], real_y[i])
+        if pred_y[i] == real_y[i]:
+            count += 1
+    print(count/len(real_y))
+
+
+
 if __name__ == "__main__":
-    verify()
+    train()
